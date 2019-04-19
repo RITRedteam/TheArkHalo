752f5f3ead4913260c2f96b23468300cf68009e3
micahjmartin <mjm5097@rit.edu>
2019-04-19 19:12:43 -0400

    cleanup

--- test.py
+++
@@ -1,9 +0,0 @@
-from arkclient import ArkClient
-import os
-
-client = ArkClient(os.environ.get("THEARK_SERVER", "http://0.0.0.0:5000"))
-
-name = "CrowdControl"
-if not client.login("admin", "password"):
-    raise Exception("Couldnt log in")
-addrs = client.getAddresses(name)
