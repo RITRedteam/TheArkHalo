1c6e63d79e83049262b7794b4b36180b908febde
Micah J. Martin <mjm5097@rit.edu>
2019-11-09 01:45:09 -0500

    Updated environment var

--- netassign.py
+++ netassign.py
@@ -35,7 +35,7 @@ def _addVirtualInterface(ip, dev):
     label = "{}:{}{}".format(dev, LABEL, random.randint(1, 1000))
     while label in _getInterfaceLabels(dev):
         label = "{}:{}{}".format(dev, LABEL, random.randint(1, 1000))
-    netmask = os.environ.get("INTERFACE_NAME", "/20")
+    netmask = os.environ.get("INTERFACE_NETMASK", "/20")
     # Add the interface
     command = "ip addr add {}{} brd + dev {} label {}"
     command = command.format(ip, netmask, dev, label)
