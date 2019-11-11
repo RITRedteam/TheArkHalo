902e1c1de5f243eb54d17f487c44e694916783cb
micahjmartin <mjm5097@rit.edu>
2019-11-10 23:07:46 -0500

    fixing cleanup

--- halo/entrypoint.sh
+++ halo/entrypoint.sh
@@ -1,4 +1,12 @@
-trap 'true' SIGTERM
+#!/bin/bash
+
+function cleanup() {
+    # Delete all the old interfaces
+    echo "Deleting interfaces"
+    python3 cleanup.py
+}
+
+trap 'cleanup' SIGTERM
 
 "${@}" &
 python3 haloGenerate.py
@@ -6,7 +14,3 @@ nginx -g "daemon off;" &
 
 #Wait
 wait $!
-
-# Delete all the old interfaces
-echo "Deleting interfaces"
-python3 cleanup.py
