793487426e7f0960f96da884f5bd03ff3092221a
micahjmartin <mjm5097@rit.edu>
2019-11-10 22:56:15 -0500

    adding cleanup

---
+++ halo/cleanup.py
@@ -0,0 +1,18 @@
+import os
+from netassign import _getIp, _getInterfaceNameFromIp, _getSubnetMaskFromIp, _delAllInterfaces
+
+device = os.environ.get("INTERFACE_NAME", "")
+if not device:
+    # In this situation, we get the IP address that the target IP would be connected with
+    # not the default gateway
+    # I realize this function will error if there are no IPs, but that _shouldnt_ happen
+    ip = _getIp()
+    device = _getInterfaceNameFromIp(ip)
+    netmask = _getSubnetMaskFromIp(ip)
+    print("[+] Detected interface: {}: {}{}".format(device, ip, netmask))
+else:
+    netmask = os.environ.get("INTERFACE_MASK", "/24")
+
+# Delete any interfaces using the IP address
+print("[*] Deleting old interfaces....")
+_delAllInterfaces(device)
--- halo/entrypoint.sh
+++ halo/entrypoint.sh
@@ -1,1 +1,12 @@
-python3 haloGenerate.py && nginx -g "daemon off;"
+trap 'true' SIGTERM
+
+"${@}" &
+python3 haloGenerate.py
+nginx -g "daemon off;" &
+
+#Wait
+wait $!
+
+# Delete all the old interfaces
+echo "Deleting interfaces"
+python3 cleanup.py
